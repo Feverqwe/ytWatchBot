680d5aea49b584e2e26fa38e973e3f0f8329b945
Anton <anton9121@gmail.com>
2016-05-10 13:29:56 +0500

    fix

--- services/youtube.js
+++ services/youtube.js
@@ -192,7 +192,7 @@ Youtube.prototype.clean = function(channelNameList) {
 
     Object.keys(this.config.channelInfo).forEach(function (channelId) {
         if (channelIdList.indexOf(channelId) === -1) {
-            debug('Removed from channelInfo %s %j', channelId, this.config.channelInfo[channelId]);
+            debug('Removed from channelInfo %s %j', channelId, _this.config.channelInfo[channelId]);
             _this.removeChannelInfo(channelId);
         }
     });
