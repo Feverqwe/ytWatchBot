777b283706c134092fe9ed98f3db7aede66db03a
Anton <anton9121@gmail.com>
2016-05-22 11:37:52 +0500

    1

--- checker.js
+++ checker.js
@@ -243,11 +243,13 @@ Checker.prototype.getPicId = function(chatId, text, stream) {
 
 Checker.prototype.getPicIdCache = function (chatId, text, stream) {
     var cache = this.requestPhotoCache;
-    return cache[stream._videoId] = this.getPicId(chatId, text, stream).then(function (msg) {
-        delete cache[stream._videoId];
+    var id = stream._videoId;
+    
+    return cache[id] = this.getPicId(chatId, text, stream).then(function (msg) {
+        delete cache[id];
         return msg;
     }).catch(function (e) {
-        delete cache[stream._videoId];
+        delete cache[id];
         throw e;
     });
 };
