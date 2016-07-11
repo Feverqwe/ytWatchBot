87d1416cf7fe7b85095b40362d367a812dd297b5
Anton <anton9121@gmail.com>
2016-07-11 05:57:56 +0500

    arrow

--- commands.js
+++ commands.js
@@ -18,7 +18,7 @@ var menuBtnList = function (page) {
             ],
             [
                 {
-                    text: '<',
+                    text: '⬅️',
                     callback_data: '/menu_page 0'
                 },
                 {
@@ -49,7 +49,7 @@ var menuBtnList = function (page) {
                     callback_data: '/delete'
                 },
                 {
-                    text: '>',
+                    text: '➡️',
                     callback_data: '/menu_page 1'
                 }
             ]
