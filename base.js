a2301ee6010265be19caa42103020f4d48186f53
Anton <anton9121@gmail.com>
2016-10-02 15:23:53 +0500

    Quote error

--- base.js
+++ base.js
@@ -267,6 +267,8 @@ module.exports.Quote = function (callPerSecond) {
                 return Promise.try(function() {
                     return cb.apply(null, args);
                 }).then(resolve).catch(reject);
+            }).catch(function (err) {
+                debug('Quote error', err);
             });
         });
 
