5f4fb21b752e0c9a282111111e230505c1da44fa
Anton <anton9121@gmail.com>
2017-02-19 11:39:48 +0500

    1

--- chat.js
+++ chat.js
@@ -238,7 +238,9 @@ Chat.prototype.onCallbackQuery = function (callbackQuery) {
     return commandFunc.apply(this, args).then(function () {
         return _this.gOptions.bot.answerCallbackQuery(callbackQuery.id, '...');
     }).catch(function(err) {
-        debug('Execute callback query command %s error!', action, err);
+        if (!/message is not modified/.test(err.message)) {
+            debug('Execute callback query command %s error!', action, err);
+        }
     }).then(function() {
         _this.track(origMsg, action)
     });
