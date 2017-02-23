5335b11c084ac35caee0984c13861084a6c05364
Anton <anton9121@gmail.com>
2017-02-23 10:04:31 +0500

    1

--- base.js
+++ base.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const path = require('path');
 const debug = require('debug')('app:base');
 const Storage = require('./storage');
-const Promise = require('bluebird');
 
 var utils = {};
 /**
