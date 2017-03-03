61c5fd6ae4ab74f23ccf59031c8b0a2a6584060d
Anton <anton9121@gmail.com>
2017-03-03 07:35:35 +0500

    simple router

--- router.js
+++ router.js
@@ -1,8 +1,9 @@
 /**
  * Created by anton on 02.03.17.
  */
+var debug = require('debug')('app:router');
 
-const _messageTypes = [
+const messageTypes = [
     'text', 'audio', 'document', 'photo', 'sticker', 'video', 'voice', 'contact',
     'location', 'new_chat_participant', 'left_chat_participant', 'new_chat_title',
     'new_chat_photo', 'delete_chat_photo', 'group_chat_created'
@@ -15,61 +16,169 @@ var Router = function (options) {
     options.bot.on('callback_query', this.handle.bind(this, 'callback_query'));
 };
 
+/**
+ * @param {string} event
+ * @param {{}} message
+ * @return {{event: string, message: {}}}
+ */
+Router.prototype.getRequest = function (event, message) {
+    return {
+        event: event,
+        message: message
+    }
+};
+
+/**
+ * @param {string} event
+ * @param {{}} message
+ */
 Router.prototype.handle = function (event, message) {
     var _this = this;
     var index = 0;
+    var req = _this.getRequest(event, message);
     var next = function () {
         var route = _this.stack[index];
-        var details = route.details;
+        if (!route) return;
 
-        if (!details.event) {
-            route.dispatch(message, next);
+        req.params = route.match(message);
+        if (!req.params) {
+            return next();
+        }
+
+        if (!route.event) {
+            route.dispatch(req, next);
         } else
-        if (details.event === event) {
-            if (!details.type) {
-                route.dispatch(message, next);
+        if (route.event === event) {
+            if (!route.type) {
+                route.dispatch(req, next);
             } else
-            if (details.type && message[details.type]) {
-                route.dispatch(message, next);
+            if (message[route.type]) {
+                route.dispatch(req, next);
             }
         }
     };
     next();
 };
 
+/**
+ * @param {{}} details
+ * @param {string} details.event
+ * @param {string} details.type
+ * @param {RegExp} re
+ * @param {function(Object, function())} callback
+ * @constructor
+ */
 var Route = function (details, re, callback) {
-    if (typeof re === 'function') {
-        callback = re;
-        re = null;
-    }
     this.re = re;
-    this.details = details;
-    this.dispatch = function (message, next) {
-        callback(message, next);
+    this.event = details.event;
+    this.type = details.type;
+    this.dispatch = function (req, next) {
+        callback(req, next);
     };
 };
 
+/**
+ * @param {{}} message
+ * @return {[]|null}
+ */
+Route.prototype.match = function (message) {
+    if (!this.re) {
+        return [];
+    }
+
+    var text = null;
+    if (this.event === 'message') {
+        text = message.text;
+    } else
+    if (this.event === 'callback_query') {
+        text = message.data;
+    }
+
+    if (!text) {
+        debug('Text is empty!');
+    }
+
+    var params = this.re.exec(text);
+    if (params) {
+        params = params.shift();
+    }
+    return params;
+};
+
+/**
+ * @param {[]} args
+ * @return {{re: RegExp, callbackList: [function]}}
+ */
+Router.prototype.prepareArgs = function (args) {
+    var re = args[0];
+    var callbackList = [].slice.call(args, 1);
+    if (typeof re === 'function') {
+        callbackList.unshift(re);
+        re = null;
+    }
+    return {
+        re: re,
+        callbackList: callbackList
+    }
+};
+
+/**
+ * @param {RegExp} [re]
+ * @param {function} callback
+ */
 Router.prototype.all = function (re, callback) {
-    this.stack.push(new Route({}, re, callback));
+    var _this = this;
+    var args = _this.prepareArgs(arguments);
+
+    args.callbackList.forEach(function (callback) {
+        _this.stack.push(new Route({}, args.re, callback));
+    });
 };
 
+/**
+ * @param {RegExp} [re]
+ * @param {function} callback
+ */
 Router.prototype.message = function (re, callback) {
-    this.stack.push(new Route({
-        event: 'message'
-    }, re, callback));
+    var _this = this;
+    var args = _this.prepareArgs(arguments);
+
+    args.callbackList.forEach(function (callback) {
+        _this.stack.push(new Route({
+            event: 'message'
+        }, re, callback));
+    });
 };
 
-_messageTypes.forEach(function (type) {
+messageTypes.forEach(function (type) {
+    /**
+     * @param {RegExp} [re]
+     * @param {function} callback
+     */
     Router.prototype[type] = function (re, callback) {
-        this.stack.push(new Route({
-            event: 'message',
-            type: type
-        }, re, callback));
+        var _this = this;
+        var args = _this.prepareArgs(arguments);
+
+        args.callbackList.forEach(function (callback) {
+            _this.stack.push(new Route({
+                event: 'message',
+                type: type
+            }, re, callback));
+        });
     };
 });
 
+/**
+ * @param {RegExp} [re]
+ * @param {function} callback
+ */
 Router.prototype.callback_query = function (re, callback) {
-    this.stack.push(new Route({
-        event: 'callback_query'
-    }, re, callback));
+    var _this = this;
+    var args = _this.prepareArgs(arguments);
+
+    args.callbackList.forEach(function (callback) {
+        _this.stack.push(new Route({
+            event: 'callback_query'
+        }, re, callback));
+    });
 };
