acdac46b02ba2080ea8875b6c78eb54fa479d2b6
Anton <anton9121@gmail.com>
2017-03-09 08:10:08 +0500

    fix

--- chat.js
+++ chat.js
@@ -668,7 +668,9 @@ var Chat = function(options) {
             _details.chat_id = chatId;
             _details.message_id = messageId;
             return bot.editMessageText(text, _details).catch(function (err) {
-                if (/message can't be edited/.test(err.message)) {
+                if (/message can't be edited/.test(err.message) ||
+                    /message to edit not found/.test(err.message)
+                ) {
                     return sendMessage();
                 }
                 throw err;
