6f7222d3c36fe501b4a17fb7cc7d5696e0e8b54b
Anton <anton9121@gmail.com>
2017-06-24 15:58:26 +0500

    rm migrate

--- index.js
+++ index.js
@@ -80,7 +80,6 @@ const options = {
 
         return options.msgStack.onReady;
     }).then(function() {
-        throw new Error('migrate...');
         options.daemon = new Daemon(options);
     }).then(function() {
         options.bot = new TelegramBot(options.config.token, {
