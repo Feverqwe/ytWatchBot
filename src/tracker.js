89db1c71d5d31c722b5f5a6fba7d59a84d5e0d73
Anton <leonardspbox@gmail.com>
2019-06-27 14:31:42 +0500

    tracker batch

--- src/tracker.js
+++ src/tracker.js
@@ -1,9 +1,16 @@
 import withRetry from "./tools/withRetry";
+import parallel from "./tools/parallel";
+import arrayByPart from "./tools/arrayByPart";
 
 const debug = require('debug')('app:tracker');
 const got = require('got');
+const qs = require('querystring');
 const uuidV4 = require('uuid/v4');
 const QuickLRU = require('quick-lru');
+const throttle = require('lodash.throttle');
+const promiseLimit = require('promise-limit');
+
+const oneLimit = promiseLimit(1);
 
 class Tracker {
   constructor(/**Main*/main) {
@@ -16,21 +23,51 @@ class Tracker {
       tid: this.tid,
       an: 'bot',
       aid: 'bot'
-    }
+    };
+
+    this.queue = [];
   }
 
   track(chatId, params) {
     const cid = this.getUuid(chatId);
 
-    return withRetry({count: 3, timeout: 250}, () => {
-      return got.post('https://www.google-analytics.com/collect', {
-        body: Object.assign({cid}, this.defaultParams, params),
-        form: true,
-      });
-    }).catch((err) => {
-      debug('track error: %o', err);
-    });
+    this.queue.push([Date.now(), Object.assign({cid}, this.defaultParams, params)]);
+
+    this.sendDataThrottled();
   }
+
+  sendData = () => {
+    return oneLimit(async () => {
+      while (this.queue.length) {
+        const queue = this.queue.splice(0);
+        await parallel(10, arrayByPart(queue, 20), (queue) => {
+          return withRetry({count: 3, timeout: 250}, () => {
+            return got.post('https://www.google-analytics.com/batch', {
+              headers: {
+                'Content-Type': 'text/html'
+              },
+              body: queue.map(([time, hit]) => {
+                hit.qt = Date.now() - time;
+                return qs.stringify(hit);
+              }).join('\n'),
+            });
+          }).catch((err) => {
+            debug('track error: %o', err);
+            const fourHoursAgo = new Date();
+            fourHoursAgo.setHours(fourHoursAgo.getHours() - 4);
+            queue.forEach(([time, hit]) => {
+              if (time > fourHoursAgo.getTime()) {
+                this.queue.unshift([time, hit]);
+              }
+            });
+          });
+        });
+      }
+    });
+  };
+  sendDataThrottled = throttle(this.sendData, 1000, {
+    leading: false
+  });
 
   getUuid(chatId) {
     if (this.lru.has(chatId)) {
