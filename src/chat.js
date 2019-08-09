b6bf546e7c080de9333e7d05aed2947b60a3ffb4
Anton <leonardspbox@gmail.com>
2019-08-09 13:01:43 +0500

    fix

--- src/chat.js
+++ src/chat.js
@@ -58,6 +58,10 @@ class Chat {
       }
     });
 
+    this.router.callback_query((req, res, next) => {
+      return this.main.bot.answerCallbackQuery(req.callback_query.id).then(next);
+    });
+
     this.router.textOrCallbackQuery((req, res, next) => {
       if (['group', 'supergroup'].includes(req.chatType)) {
         return promiseTry(() => {
@@ -107,10 +111,6 @@ class Chat {
           t: 'event',
         });
       }
-    });
-
-    this.router.callback_query((req, res, next) => {
-      return this.main.bot.answerCallbackQuery(req.callback_query.id).then(next);
     });
 
     this.router.text(/\/ping/, (req, res) => {
