5f9925aead6fe58605e22a96e38f6f0de0bcee8c
Anton <leonardspbox@gmail.com>
2019-12-20 10:17:59 +0500

    1

--- src/chat.js
+++ src/chat.js
@@ -174,7 +174,7 @@ class Chat {
         lines.push('');
         lines.push(`${name}:`);
         serviceTopChannels.forEach(({title, chatCount}, index) => {
-          lines.push(chatCount + '. ' + title);
+          lines.push(chatCount + ' - ' + title);
         });
 
         return this.main.bot.sendMessage(req.chatId, lines.join('\n'), {
