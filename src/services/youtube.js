e1e9584cbfdc97eea3f114b4ea803ce2f6c7a48e
Anton <leonardspbox@gmail.com>
2019-12-20 09:13:33 +0500

    add requestChannelIdByUserUrl mask

--- src/services/youtube.js
+++ src/services/youtube.js
@@ -270,7 +270,8 @@ class Youtube {
     let username = null;
     [
       /youtube\.com\/(?:#\/)?user\/([\w\-]+)/i,
-      /youtube\.com\/([\w\-]+)/i
+      /youtube\.com\/([\w\-]+)/i,
+      /youtube\.com\/c\/([\w\-]+)/i,
     ].some((re) => {
       const m = re.exec(url);
       if (m) {
