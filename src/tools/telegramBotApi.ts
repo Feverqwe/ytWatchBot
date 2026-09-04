f19a7b15d3fdb9006658120e25daa7ce129481fd
Anton <Feverqwe@users.noreply.github.com>
2026-09-04 20:49:15 +0300

    Use Telegram API v2 rate limiter

--- src/tools/telegramBotApi.ts
+++ src/tools/telegramBotApi.ts
@@ -1,20 +1,27 @@
-import {Bot, type Api} from 'node-telegram-bot-api';
-import RateLimit2 from './rateLimit2';
+import {Bot, RateLimiter, type Api} from 'node-telegram-bot-api';
 import {getDebug} from './getDebug';
 
 const debug = getDebug('app:telegramBotApi');
 
 export function applyTelegramRateLimits(api: Api): void {
-  const sendLimit = new RateLimit2(30);
-  const chatActionLimit = new RateLimit2(30);
+  const sendLimit = new RateLimiter({global: 30});
+  const chatActionLimit = new RateLimiter({global: 30});
   const sendChatAction = api.sendChatAction.bind(api);
   const sendMessage = api.sendMessage.bind(api);
   const sendPhoto = api.sendPhoto.bind(api);
 
-  api.sendChatAction = (params, signal) =>
-    chatActionLimit.run(() => sendChatAction(params, signal));
-  api.sendMessage = (params, signal) => sendLimit.run(() => sendMessage(params, signal));
-  api.sendPhoto = (params, signal) => sendLimit.run(() => sendPhoto(params, signal));
+  api.sendChatAction = async (params, signal) => {
+    await chatActionLimit.acquire(params.chat_id, signal);
+    return sendChatAction(params, signal);
+  };
+  api.sendMessage = async (params, signal) => {
+    await sendLimit.acquire(params.chat_id, signal);
+    return sendMessage(params, signal);
+  };
+  api.sendPhoto = async (params, signal) => {
+    await sendLimit.acquire(params.chat_id, signal);
+    return sendPhoto(params, signal);
+  };
 }
 
 export const getTelegramBot = (token: string): Bot => {
